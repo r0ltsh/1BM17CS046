203e28d35d830e35dc9730a0cc5c600ee8e6c705
r0ltsh <51821630+r0ltsh@users.noreply.github.com>
2019-10-23 12:47:17 +0530

    Add files via upload

---
+++ tcpServer.py
@@ -0,0 +1,15 @@
+from socket import *
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 12000
+serverName = "10.124.6.26"
+serverSocket.bind((serverName, serverPort))
+serverSocket.listen(1)
+
+while(True):
+	conn, addr = serverSocket.accept()
+	fileName = conn.recv(1024)
+	with open(fileName.decode(), "r") as f:
+		message = f.read()
+	conn.send(message.encode())
+	conn.close()
